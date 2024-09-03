149b7e5526fe5db35b18112ccd841b700b5c7096
curjlync <curjlync@ut.utm.edu>
2024-09-03 16:34:05 -0500

    initial datastore commit

---
+++ pythonScripts/datastore.py
@@ -0,0 +1,15 @@
+# DataStore Python Library
+# Curt Lynch
+# This library allows for the storage and retreival of synchronized data signals to/from simple CSV files.
+
+# File Layout
+'''
+signalname0,signalname1,signalname2,...
+signalfrequency0,signalfrequency1.signalfrequency2,...
+
+signal0data
+signal1data
+signal2data
+...
+
+'''     
