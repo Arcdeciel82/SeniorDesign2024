64167d507c94591ca1ea30cf6d88671da0db8d80
curjlync <curjlync@ut.utm.edu>
2024-09-16 15:38:06 -0500

    significant rewrite

---
+++ pythonScripts/collection.py
@@ -0,0 +1,19 @@
+# Collection.py
+
+
+class dataCollection:
+    """base class for different collection sources."""
+    def startCollection(self):
+        pass
+    
+    def stopCollection(self):
+        pass
+
+class tobiCollection(dataCollection):
+    """Insert Description here."""
+
+class cytonCollection(dataCollection):
+    """Insert Description here."""
+
+class ue5Collection(dataCollection):
+    """Insert Description here."""
---
+++ pythonScripts/simulationUI.py
@@ -0,0 +1,29 @@
+# simulationUI.py
+# Curt Lynch
+# This program will host a ui that will help with collecting and storing data from CytonDaisy, UE5, and Tubi.
+
+from nicegui import ui
+from nicegui.events import ValueChangeEventArguments
+from collection import cytonCollection, ue5Collection, tobiCollection
+
+
+def CollectionSwitch(event:ValueChangeEventArguments, agent):
+    if event.value:
+        ui.notify(f"Starting {event.sender._text}!")
+        agent.startCollection()
+    else:
+        ui.notify(f"Stopping {event.sender._text}!")
+        agent.stopCollection()
+
+cyton = cytonCollection()
+ue5 = ue5Collection()
+tobi = tobiCollection()
+
+ui.dark_mode(True)
+ui.page_title("Data Collection")
+with ui.row():
+    ui.switch('Cyton Collection', on_change=lambda e: CollectionSwitch(e, cyton))
+    ui.switch('Tobi Collection', on_change=lambda e: CollectionSwitch(e, tobi))
+    ui.switch('UE5 Simulation', on_change=lambda e: CollectionSwitch(e, ue5))
+
+ui.run()
